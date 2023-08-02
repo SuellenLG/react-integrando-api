569f31d59a8f44e626676e9f6d72b1f5341970e3
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-08-02 15:47:51 -0300

    Delete, put, post, get

--- alfood-main/src/paginas/Adminstracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ alfood-main/src/paginas/Adminstracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Button } from '@mui/material';
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const AdministracaoRestaurantes = () => {
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
@@ -10,7 +11,17 @@ const AdministracaoRestaurantes = () => {
 
         axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
             .then(resposta => setRestaurantes(resposta.data))
-    })
+
+    }, [])
+
+    const excluir = (restauranteAhSerExcluido: IRestaurante) => {
+
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+            .then(() => {
+                const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAhSerExcluido.id)
+                setRestaurantes([...listaRestaurante])
+            })
+    }
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -19,6 +30,12 @@ const AdministracaoRestaurantes = () => {
                         <TableCell>
                             Nome
                         </TableCell>
+                        <TableCell>
+                            Editar
+                        </TableCell>
+                        <TableCell>
+                            Excluir
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -26,6 +43,14 @@ const AdministracaoRestaurantes = () => {
                         <TableRow key={restaurante.id}>
                             <TableCell>
                                 {restaurante.nome}
+                            </TableCell>
+                            <TableCell>
+                                <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                                    Excluir
+                                </Button>
                             </TableCell>
                         </TableRow>
                     )}
--- alfood-main/src/paginas/Adminstracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood-main/src/paginas/Adminstracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,22 +1,44 @@
 import { Button, TextField } from "@mui/material"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import IRestaurante from "../../../interfaces/IRestaurante"
 
 const FormularioRestaurante = () => {
 
+    const parametros = useParams();
+
+    useEffect(() => {
+        if (parametros.id) {
+            axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        }
+    }, [parametros])
+
     const [nomeRestaurante, setNomeRestaurante] = useState('')
 
-    const aoSubmiterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault()
-        axios.post('http://localhost:8000/api/v2/restaurantes/', {
-            nome: nomeRestaurante
-        })
-            .then(() => {
-                alert("restaurante cadastrado com sucesso!")
+
+        if (parametros.id) {
+            axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+                
+                nome: nomeRestaurante
             })
+                .then(() => {
+                    alert("restaurante atualizado com sucesso!")
+                })
+        } else {
+            axios.post('http://localhost:8000/api/v2/restaurantes/', {
+                nome: nomeRestaurante
+            })
+                .then(() => {
+                    alert("restaurante cadastrado com sucesso!")
+                })
+        }
     }
 
-    return (<form onSubmit={aoSubmiterForm}>
+    return (<form onSubmit={aoSubmeterForm}>
         <TextField
             value={nomeRestaurante}
             onChange={evento => setNomeRestaurante(evento.target.value)}
@@ -33,3 +55,5 @@ const FormularioRestaurante = () => {
 }
 
 export default FormularioRestaurante
+
+
